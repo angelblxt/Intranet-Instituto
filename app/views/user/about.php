3e45a47e9aef1e2549d78835b056ff7ab029f500
angelblxt <angelquerolgarcia@gmail.com>
2015-01-15 16:56:15 +0100

    Contenido de la sección "Acerca".

---
+++ app/views/user/about.php
@@ -0,0 +1,23 @@
+<center>
+
+	<div class="about_logo"></div><br/>
+
+	<div class="about_title">Intranet</div>
+	<div class="about_subtitle">I.E.S Benjamín Jarnés (sección Fuentes de Ebro)</div>
+
+	<div class="about_developer">Desarrollado por <b>Ángel Querol García</b></div>
+
+	<div class="about_socialNetworks">
+
+		<a href="mailto:angelquerolgarcia@gmail.com" target="_blank" class="about_socialButton"><i class="fa fa-envelope-square"></i></a>
+		<a href="https://www.facebook.com/angel.querolgarcia" target="_blank" class="about_socialButton"><i class="fa fa-facebook-square"></i></a>
+		<a href="https://twitter.com/angelblxt" target="_blank" class="about_socialButton"><i class="fa fa-twitter-square"></i></a>
+		<a href="https://instagram.com/angelquerol" target="_blank" class="about_socialButton"><i class="fa fa-instagram"></i></a>
+		<a href="https://plus.google.com/118236736571237059283" target="_blank" class="about_socialButton"><i class="fa fa-google-plus-square"></i></a>
+		<a href="https://es.linkedin.com/pub/ángel-querol-garcía/66/a73/a75/" target="_blank" class="about_socialButton"><i class="fa fa-linkedin-square"></i></a>
+		<a href="https://github.com/angelblxt" target="_blank" class="about_socialButton"><i class="fa fa-git-square"></i></a>
+		<a href="skype:angelblxt" target="_blank" class="about_socialButton"><i class="fa fa-skype"></i></a>
+
+	</div>
+
+</center>
