7134135c266d4e6e2c85c258263b52dc16d21318
angelblxt <angelquerolgarcia@gmail.com>
2015-01-15 16:05:53 +0100

    Dar funcionalidad al Menú izquierdo.

--- app/templates/user/aside.php
+++ app/templates/user/aside.php
@@ -1,3 +1,31 @@
+<?php
+
+	$actual = \helpers\url::actual();
+
+	if(preg_match('/^user*/i', $actual)){
+
+		$userSelected = 'seleccionado';
+
+	} elseif(preg_match('/^folders*/i', $actual)){
+
+		$foldersSelected = 'seleccionado';
+
+	} elseif(preg_match('/^messages*/i', $actual)){
+
+		$messagesSelected = 'seleccionado';
+
+	} elseif(preg_match('/^preferences*/i', $actual)){
+
+		$preferencesSelected = 'seleccionado';
+
+	} elseif(preg_match('/^about*/i', $actual)){
+
+		$aboutSelected = 'seleccionado';
+
+	}
+
+?>
+
 <aside>
 					
 	<div class="topZone">
@@ -11,27 +39,27 @@
 	</div>
 	<div class="menu">
 
-		<div class="boton seleccionado">
+		<div class="boton <?php echo $userSelected ?>">
 							
 			<span class="hint--rounded hint--bounce hint--right" data-hint="Inicio"><a href="#"><i class="fa fa-home"></i></a></span>
 						
 		</div>
-		<div class="boton">
+		<div class="boton <?php echo $foldersSelected ?>">
 							
 			<span class="hint--rounded hint--bounce hint--right" data-hint="Carpetas"><a href="#"><i class="fa fa-folder"></i></a></span>
 
 		</div>
-		<div class="boton">
+		<div class="boton <?php echo $messagesSelected ?>">
 							
 			<span class="hint--rounded hint--bounce hint--right" data-hint="Mensajes Privados"><a href="#"><i class="fa fa-envelope"></i></a></span>
 
 		</div>
-		<div class="boton">
+		<div class="boton <?php echo $preferencesSelected ?>">
 							
 			<span class="hint--rounded hint--bounce hint--right" data-hint="Preferencias"><a href="#"><i class="fa fa-cogs"></i></a></span>
 
 		</div>
-		<div class="boton">
+		<div class="boton <?php echo $aboutSelected ?>">
 							
 			<span class="hint--rounded hint--bounce hint--right" data-hint="Acerca De"><a href="#"><i class="fa fa-info-circle"></i></a></span>
 
