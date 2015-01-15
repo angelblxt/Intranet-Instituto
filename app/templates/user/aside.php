774cddd88b3546ddda618573d8d23ab2a1fa25d4
angelblxt <angelquerolgarcia@gmail.com>
2015-01-15 15:40:58 +0100

    Sección del Usuario y Cierre de Sesión.

---
+++ app/templates/user/aside.php
@@ -0,0 +1,46 @@
+<aside>
+					
+	<div class="topZone">
+
+		<div class="circle" style="background-color: #607d8b"><?php echo $inicial ?></div>
+
+		<div class="logout"><a href="user/logout"><i class="fa fa-sign-out"></i> Salir</a></div>
+
+		<div class="mediumBar"></div>
+
+	</div>
+	<div class="menu">
+
+		<div class="boton seleccionado">
+							
+			<span class="hint--rounded hint--bounce hint--right" data-hint="Inicio"><a href="#"><i class="fa fa-home"></i></a></span>
+						
+		</div>
+		<div class="boton">
+							
+			<span class="hint--rounded hint--bounce hint--right" data-hint="Carpetas"><a href="#"><i class="fa fa-folder"></i></a></span>
+
+		</div>
+		<div class="boton">
+							
+			<span class="hint--rounded hint--bounce hint--right" data-hint="Mensajes Privados"><a href="#"><i class="fa fa-envelope"></i></a></span>
+
+		</div>
+		<div class="boton">
+							
+			<span class="hint--rounded hint--bounce hint--right" data-hint="Preferencias"><a href="#"><i class="fa fa-cogs"></i></a></span>
+
+		</div>
+		<div class="boton">
+							
+			<span class="hint--rounded hint--bounce hint--right" data-hint="Acerca De"><a href="#"><i class="fa fa-info-circle"></i></a></span>
+
+		</div>
+
+	</div>
+
+</aside>
+
+<div class="contenido">
+					
+	<div class="box">
---
+++ app/templates/user/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+	
+	<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+
+		<head>
+
+			<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+			<title> <?php echo $title .' - '. SITETITLE ?> </title>
+
+			<?php
+
+				// Archivos CSS.
+
+					\helpers\assets::css(array(
+						\helpers\url::template_path() . 'css/font-awesome/css/font-awesome.min.css',
+						\helpers\url::template_path() . 'css/ui.css',
+						\helpers\url::template_path() . 'css/buttons.css',
+						\helpers\url::template_path() . 'css/forms.css',
+						\helpers\url::template_path() . 'css/messages.css',
+						\helpers\url::template_path() . 'css/switch.css',
+						\helpers\url::template_path() . 'css/tables.css',
+						\helpers\url::template_path() . 'css/hint.min.css'
+					));
+
+				// Archivos JS.
+
+					\helpers\assets::js(array(
+						\helpers\url::template_path() . 'js/prefixfree.min.js',
+						\helpers\url::template_path() . 'js/jquery.min.css',
+					));
+
+			?>
+
+			<!-- <link rel="icon" href="<?php echo IMG; ?>favicon.ico" sizes="16x16 24x24 32x32 64x64" type="image/vnd.microsoft.icon"> -->
+
+		</head>
+
+		<body>
+
+			<div class="container">
