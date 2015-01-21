c7c94977bd4f8ac987ed2aadb1623a92d6952a3c
angelblxt <angelquerolgarcia@gmail.com>
2015-01-21 19:56:26 +0100

    Diseño provisional de los archivos.

---
+++ app/views/user/folders/folders.php
@@ -0,0 +1,27 @@
+<div class="folders_file">
+
+	<div class="icono">
+
+		<div class="file-icon" data-type="xls"></div>
+
+	</div>
+
+	<div class="nombre file">archivo.ext</div>
+
+	<div class="opciones">
+
+		<div class="size">1.9 KB</div>
+		
+		<span class="hint--rounded hint--bounce hint--bottom" data-hint="Descargar">
+			<button onClick="location.href=''" class="button button-rounded button-flat-primary button-tiny"><i class="fa fa-download" style="margin: 0 -10px"></i></button>
+		</span>
+		<span class="hint--rounded hint--bounce hint--bottom" data-hint="Renombrar">
+			<button onClick="location.href=''" class="button button-rounded button-flat-action button-tiny"><i class="fa fa-pencil" style="margin: 0 -10px"></i></button>
+		</span>
+		<span class="hint--rounded hint--bounce hint--bottom" data-hint="Eliminar">
+			<button onClick="location.href=''" class="button button-rounded button-flat-caution button-tiny"><i class="fa fa-times-circle" style="margin: 0 -10px"></i></button>
+		</span>
+	
+	</div>
+
+</div>
