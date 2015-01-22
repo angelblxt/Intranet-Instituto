6c41c13f3fc7309df7079433d0aab4867d0ba82d
angelblxt <angelquerolgarcia@gmail.com>
2015-01-22 18:00:59 +0100

    Botón de opciones por archivo/carpeta.

--- app/views/user/folders/folders.php
+++ app/views/user/folders/folders.php
@@ -31,13 +31,16 @@
 			<div class="size"> <?php echo $file['size'] ?> </div>
 			
 			<span class="hint--rounded hint--bounce hint--bottom" data-hint="Descargar">
-				<button onClick="location.href=''" class="button button-rounded button-flat-primary button-tiny"><i class="fa fa-download" style="margin: 0 -10px"></i></button>
+				<button onClick="location.href=''" class="button button-rounded button-flat-primary button-tiny" style="padding: 0 15px"><i class="fa fa-download" style="margin: 0 -10px"></i></button>
 			</span>
 			<span class="hint--rounded hint--bounce hint--bottom" data-hint="Renombrar">
-				<button onClick="location.href=''" class="button button-rounded button-flat-action button-tiny"><i class="fa fa-pencil" style="margin: 0 -10px"></i></button>
+				<button onClick="location.href=''" class="button button-rounded button-flat-action button-tiny" style="padding: 0 15px"><i class="fa fa-pencil" style="margin: 0 -10px"></i></button>
+			</span>
+			<span class="hint--rounded hint--bounce hint--bottom" data-hint="Opciones">
+				<button onClick="location.href=''" class="button button-rounded button-flat-highlight button-tiny" style="padding: 0 15px"><i class="fa fa-cog" style="margin: 0 -10px"></i></button>
 			</span>
 			<span class="hint--rounded hint--bounce hint--bottom" data-hint="Eliminar">
-				<button onClick="location.href=''" class="button button-rounded button-flat-caution button-tiny"><i class="fa fa-times-circle" style="margin: 0 -10px"></i></button>
+				<button onClick="location.href=''" class="button button-rounded button-flat-caution button-tiny" style="padding: 0 15px"><i class="fa fa-times" style="margin: 0 -10px"></i></button>
 			</span>
 		
 		</div>
