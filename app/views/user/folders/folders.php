59627b22518bfb17de631dc6d4baa5f9b513f240
angelblxt <angelquerolgarcia@gmail.com>
2015-01-22 22:49:16 +0100

    Texto si no hay elementos en una Carpeta.

--- app/views/user/folders/folders.php
+++ app/views/user/folders/folders.php
@@ -1,8 +1,10 @@
 <div class="sectionTitle"><?php echo $titleSection; ?></div>
 
-<?php \helpers\messages::comprobarErrores(); ?>
+<?php 
 
-<?php if(!empty($previous)): ?>
+\helpers\messages::comprobarErrores();
+
+if(!empty($previous)): ?>
 
 	<button onClick="location.href='<?php echo DIR ?>folders/<?php echo $previous ?>'" class="button button-rounded button-flat-primary button-tiny" style="padding: 0 10px; margin-bottom: 15px"><i class="fa fa-chevron-left"></i> Atrás</button>
 
@@ -10,41 +12,49 @@
 
 	<button onClick="location.href='<?php echo DIR ?>folders/<?php echo $actual ?>/new/folder'" class="button button-rounded button-flat-action button-tiny" style="padding: 0 10px; margin-bottom: 15px"><i class="fa fa-folder"></i> Nueva Carpeta</button>
 
-<?php foreach($files as $file): ?>
+<?php if(count($files) == 0): ?>
 
-	<div class="folders_file">
+	<div class="folders_nothing"><i class="fa fa-times-circle"></i> No hay elementos en ésta Carpeta.</div>
 
-		<div class="icono"> <?php echo $file['icon'] ?> </div>
+<?php else: ?>
 
-		<?php if($file['type'] == 'dir'): ?>
+	<?php foreach($files as $file): ?>
 
-			<a href="<?php echo DIR ?>folders/<?php echo $file['next'] ?>"><div class="nombre <?php echo $file['type'] ?>"> <?php echo $file['name']['decrypted'] ?> </div></a>
+		<div class="folders_file">
 
-		<?php else: ?>
+			<div class="icono"> <?php echo $file['icon'] ?> </div>
 
-			<div class="nombre <?php echo $file['type'] ?>"> <?php echo $file['name']['decrypted'] ?> </div>
+			<?php if($file['type'] == 'dir'): ?>
 
-		<?php endif; ?>
+				<a href="<?php echo DIR ?>folders/<?php echo $file['next'] ?>"><div class="nombre <?php echo $file['type'] ?>"> <?php echo $file['name']['decrypted'] ?> </div></a>
 
-		<div class="opciones">
+			<?php else: ?>
 
-			<div class="size"> <?php echo $file['size'] ?> </div>
+				<div class="nombre <?php echo $file['type'] ?>"> <?php echo $file['name']['decrypted'] ?> </div>
+
+			<?php endif; ?>
+
+			<div class="opciones">
+
+				<div class="size"> <?php echo $file['size'] ?> </div>
+				
+				<span class="hint--rounded hint--bounce hint--bottom" data-hint="Descargar">
+					<button onClick="location.href=''" class="button button-rounded button-flat-primary button-tiny" style="padding: 0 15px"><i class="fa fa-download" style="margin: 0 -10px"></i></button>
+				</span>
+				<span class="hint--rounded hint--bounce hint--bottom" data-hint="Renombrar">
+					<button onClick="location.href='<?php echo $file['buttons']['rename'] ?>'" class="button button-rounded button-flat-action button-tiny" style="padding: 0 15px"><i class="fa fa-pencil" style="margin: 0 -10px"></i></button>
+				</span>
+				<span class="hint--rounded hint--bounce hint--bottom" data-hint="Opciones">
+					<button onClick="location.href=''" class="button button-rounded button-flat-highlight button-tiny" style="padding: 0 15px"><i class="fa fa-cog" style="margin: 0 -10px"></i></button>
+				</span>
+				<span class="hint--rounded hint--bounce hint--bottom" data-hint="Eliminar">
+					<button onClick="location.href='<?php echo $file['buttons']['delete'] ?>'" class="button button-rounded button-flat-caution button-tiny" style="padding: 0 15px"><i class="fa fa-times" style="margin: 0 -10px"></i></button>
+				</span>
 			
-			<span class="hint--rounded hint--bounce hint--bottom" data-hint="Descargar">
-				<button onClick="location.href=''" class="button button-rounded button-flat-primary button-tiny" style="padding: 0 15px"><i class="fa fa-download" style="margin: 0 -10px"></i></button>
-			</span>
-			<span class="hint--rounded hint--bounce hint--bottom" data-hint="Renombrar">
-				<button onClick="location.href='<?php echo $file['buttons']['rename'] ?>'" class="button button-rounded button-flat-action button-tiny" style="padding: 0 15px"><i class="fa fa-pencil" style="margin: 0 -10px"></i></button>
-			</span>
-			<span class="hint--rounded hint--bounce hint--bottom" data-hint="Opciones">
-				<button onClick="location.href=''" class="button button-rounded button-flat-highlight button-tiny" style="padding: 0 15px"><i class="fa fa-cog" style="margin: 0 -10px"></i></button>
-			</span>
-			<span class="hint--rounded hint--bounce hint--bottom" data-hint="Eliminar">
-				<button onClick="location.href='<?php echo $file['buttons']['delete'] ?>'" class="button button-rounded button-flat-caution button-tiny" style="padding: 0 15px"><i class="fa fa-times" style="margin: 0 -10px"></i></button>
-			</span>
-		
+			</div>
+
 		</div>
 
-	</div>
+	<?php endforeach; ?>
 
-<?php endforeach; ?>
+<?php endif; ?>
