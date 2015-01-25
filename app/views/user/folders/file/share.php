5205349a67d1ebc6ec8b0d43ddcb70a61be5c7db
angelblxt <angelquerolgarcia@gmail.com>
2015-01-25 13:22:58 +0100

    Cambio de planificación. "Opciones Carpeta/Archivo" > "Compartir Carpeta/Archivo".

---
+++ app/views/user/folders/file/share.php
@@ -0,0 +1,5 @@
+<div class="sectionTitle">Compartir Archivo</div>
+
+<?php \helpers\messages::comprobarErrores(); ?>
+
+<div class="folders_showName"><i class="fa fa-file-o"></i> <?php echo $file['decrypted'] ?></div>
