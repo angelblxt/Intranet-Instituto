b1e8728a89debb3811e63cc38124c166a531cf3f
angelblxt <angelquerolgarcia@gmail.com>
2015-01-29 23:07:23 +0100

    Mejoras en las Comparticiones.

---
+++ app/controllers/shared.php
@@ -0,0 +1,226 @@
+<?php namespace controllers;
+use core\view,
+	helpers\nocsrf as NoCSRF,
+	helpers\url as Url,
+	helpers\session as Session,
+	helpers\security as Seguridad,
+	helpers\filesystem as FS;
+
+class Shared extends \core\controller{
+
+	public $username;
+	public $templateData;
+
+	public function __construct(){
+
+		parent::__construct();
+
+		$this->language->load('login');
+
+		// Cargamos Modelos.
+			$this->_user    = new \models\user();
+			$this->_log     = new \models\log();
+			$this->_message = new \models\message();
+			$this->_fs      = new \models\filesystem();
+
+		if($this->_user->isLogged())
+			$this->username = Session::get('username');
+
+		// Datos del Template.
+			$nombreApellidos = $this->_user->getNameSurname();
+
+			$this->templateData = [
+				'nombre'        => $nombreApellidos,
+				'inicial'       => utf8_encode($nombreApellidos['nombre'][0]),
+				'colorCirculo'  => $this->_user->getCircleColor(),
+				'shake_message' => ($this->_message->number_unreaded() > 0)? true : false];
+
+		// Envitamos ataques.
+			foreach( $_POST as $key => $value ){
+
+				$_POST[$key] = Seguridad::cleanInput($value);
+
+			}
+
+			foreach( $_GET as $key => $value ){
+
+				$_GET[$key] = Seguridad::cleanInput($value);
+
+			}
+
+		// Iniciamos el Sistema de Archivos.
+			FS::personalFS();
+
+	}
+
+/*
+|-----------------------------------------------
+| Listado de Carpetas y Archivos
+|-----------------------------------------------
+*/
+
+	public function folders($folder = '', $userCompartidor = '')
+	{
+
+		if(!$this->_user->isLogged()){
+
+			Url::redirect('');
+
+		} else {
+
+			$this->_log->add('Ha entrado en la sección "Carpetas Privadas".');
+
+			$myHash = $this->_user->getHash($this->username);
+
+			$foldersShared = $this->_fs->getPathsSharedWithMe($myHash);
+
+			$toShow = [];
+
+			if(empty($folder) || empty($userCompartidor)){
+
+				foreach($foldersShared as $par){
+
+					$user = $this->_user->getUser($par['hash']);
+
+					FS::personalFS($user);
+
+					$anterior = FS::getAnteriorPath($par['path']);
+
+					$folders = FS::listFolders($anterior);
+
+					foreach($folders as $dir){
+
+						if($this->_fs->isSharedWithMe($par['hash'], $myHash, $dir['path'])){
+							
+							$toShow[] = [
+								'user' => $user,
+								'data' => $dir];
+						
+						}
+
+					}
+
+				}
+
+			} else {
+
+				$folder          = Seguridad::desencriptar(base64_decode($folder), 2);
+				$userCompartidor = Seguridad::desencriptar(base64_decode($userCompartidor), 2);
+
+				FS::personalFS($userCompartidor);
+
+				$anterior = FS::getAnteriorPath($folder);
+
+				$folders = FS::listFolders($folder);
+
+				foreach($folders as $dir){
+							
+					$toShow[] = [
+						'user' => $userCompartidor,
+						'data' => $dir];
+
+				}
+
+			}
+
+				$files = [];
+
+				foreach($toShow as $file){
+
+					$compartidor       = $file['user'];
+					$nombreCompartidor = $this->_user->getNameSurname($compartidor);
+
+					$extension   = FS::getExtension($file['data']['name']);
+					$size        = FS::formatBytes($file['data']['size'], 2);
+					$next        = base64_encode(Seguridad::encriptar($file['data']['path'], 2));
+					$compartidor = base64_encode(Seguridad::encriptar($file['user'], 2));
+					$isShared    = ($this->_fs->isShared($file['data']['path']))? '<i class="fa fa-share-alt" title="Compartido" style="margin-left: 10px"></i> <i>'. $nombreCompartidor['nombre'] .' '. $nombreCompartidor['apellidos'] .'</i>' : '';
+
+					if($file['data']['type'] == 'dir'){
+
+						$files[] = [
+							'name'     => $file['data']['name'] . $isShared,
+							'icon'     => '<i class="fa fa-folder"></i>',
+							'size'     => '',
+							'type'     => $file['data']['type'],
+							'next'     => $next .'/'. $compartidor,
+							'previous' => $previous];
+
+					} else {
+
+						$files[] = [
+							'name' => $file['data']['name'] . $isShared,
+							'icon' => '<div class="file-icon" data-type="'. $extension .'"></div>',
+							'size' => $size,
+							'type' => $file['data']['type'],
+							'next' => $next .'/'. $compartidor];
+
+					}
+
+				}
+
+				// $nombreCarpetaActual = str_replace('_', ' ', FS::getFolderName($folderToGo));
+
+			// FIN DEL SISTEMA DE ARCHIVOS
+
+			$data = ['title' => 'Carpetas Compartidas'];
+
+			$section = [
+				'files'        => $files,
+				'previous'     => $previous,
+				'actual'       => $actual,
+				'titleSection' => (empty($nombreCarpetaActual))? 'Carpetas Compartidas Conmigo' : $nombreCarpetaActual];
+			
+			Session::set('template', 'user');
+
+			View::rendertemplate('header', $data);
+			View::rendertemplate('topHeader', $this->templateData);
+			View::rendertemplate('aside', $this->templateData);
+			View::render('user/folders/shared', $section);
+			View::rendertemplate('footer');
+
+		}
+
+	}
+
+/*
+|-----------------------------------------------
+| Sección para Descargar Archivos o Carpetas.
+|-----------------------------------------------
+*/
+
+	public function download($file = '')
+	{
+
+		if(!$this->_user->isLogged()){
+
+			Url::redirect('');
+
+		} else {
+
+			$fileDecrypted = Seguridad::desencriptar(base64_decode($file), 2);
+
+			if(FS::comprobeFolder($fileDecrypted)){
+
+				$name = date('d-m-Y-h-i-s');
+
+				$carpetaAnterior = FS::getAnteriorPath($fileDecrypted);
+
+				if(FS::comprimeFolder($fileDecrypted, $name) === true){
+
+					if(!FS::download($name . '.zip'))
+						Url::redirect('folders/'. $carpetaAnterior);
+
+				}
+
+			} else {
+
+				FS::download($fileDecrypted, false, false);
+
+			}
+
+		}
+
+	}
+
+}
