2cd719d9c45aa0a7b02f07a776ba2f436b42d337
angelblxt <angelquerolgarcia@gmail.com>
2015-02-01 00:30:09 +0100

    Link de acceso al Panel de Administración desde el Inicio.

--- app/views/user/me.php
+++ app/views/user/me.php
@@ -1,5 +1,11 @@
 <center>
 
+	<?php if($isTeacher === true || $isAdmin === true): ?>
+
+		<div class="preferences_button brown"><a href="<?php echo DIR; ?>admin"><i class="fa fa-shield"></i><span>Administración</span></a></div>
+
+	<?php endif; ?>
+
 	<div class="preferences_button green"><a href="<?php echo DIR; ?>cloud"><i class="fa fa-folder"></i><span>Carpetas</span></a></div>
 	<div class="preferences_button blue"><a href="<?php echo DIR; ?>messages"><i class="fa fa-envelope"></i><span>Mensajes Privados</span></a></div>
 	<div class="preferences_button orange"><a href="<?php echo DIR; ?>preferences"><i class="fa fa-cogs"></i><span>Preferencias</span></a></div>
