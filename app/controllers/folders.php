9455747e78cf6b1566aa71c3031bb0a528162542
angelblxt <angelquerolgarcia@gmail.com>
2015-01-31 22:56:21 +0100

    Agrupamiento de Métodos y Optimizaciones en el código.

--- app/controllers/folders.php
+++ app/controllers/folders.php
@@ -178,6 +178,144 @@ class Folders extends \core\controller{
 
 /*
 |-----------------------------------------------
+| Listado de Carpetas y Archivos Compartidos
+|-----------------------------------------------
+*/
+
+	public function shared($folder = '', $userCompartidor = '', $father = '')
+	{
+
+		if(!$this->_user->isLogged()){
+
+			Url::redirect('');
+
+		} else {
+
+			$this->_log->add('Ha entrado en la sección "Carpetas Privadas".');
+
+			$myHash = $this->_user->getHash($this->username);
+
+			$foldersShared = $this->_fs->getPathsSharedWithMe($myHash);
+
+			$toShow = [];
+
+			if(empty($folder) || empty($userCompartidor)){
+
+				foreach($foldersShared as $par){
+
+					$user = $this->_user->getUser($par['hash']);
+
+					FS::personalFS($user);
+
+					$anterior = FS::getAnteriorPath($par['path']);
+
+					$folders = FS::listFolders($anterior);
+
+					foreach($folders as $dir){
+
+						if($this->_fs->isSharedWithMe($par['hash'], $myHash, $dir['path'])){
+							
+							$toShow[] = [
+								'user'   => $user,
+								'data'   => $dir,
+								'father' => $dir['path']];
+						
+						}
+
+					}
+
+				}
+
+			} else {
+
+				$desencriptado = [
+					'folder'          => Seguridad::desencriptar(base64_decode($folder), 2),
+					'userCompartidor' => Seguridad::desencriptar(base64_decode($userCompartidor), 2),
+					'father'          => Seguridad::desencriptar(base64_decode($father), 2)];
+
+				$desencriptado['folder'] = ($desencriptado['folder'][0] == '/')? substr($desencriptado['folder'], 1) : $desencriptado['folder'];
+
+				$previous = ($desencriptado['folder'] == $desencriptado['father'])? '' : base64_encode(Seguridad::encriptar(FS::getAnteriorPath($desencriptado['folder']), 2));
+				$previous = (empty($previous))? $previous : $previous .'/'. $userCompartidor .'/'. $father;
+
+				FS::personalFS($desencriptado['userCompartidor']);
+
+				$folders = FS::listFolders($desencriptado['folder']);
+
+				foreach($folders as $dir){
+							
+					$toShow[] = [
+						'user'   => $desencriptado['userCompartidor'],
+						'data'   => $dir,
+						'father' => $desencriptado['father']];
+
+				}
+
+			}
+
+				$files = [];
+
+				foreach($toShow as $file){
+
+					$compartidor       = $file['user'];
+					$nombreCompartidor = $this->_user->getNameSurname($compartidor);
+
+					$extension   = FS::getExtension($file['data']['name']);
+					$size        = FS::formatBytes($file['data']['size'], 2);
+					$next        = base64_encode(Seguridad::encriptar($file['data']['path'], 2));
+					$compartidor = base64_encode(Seguridad::encriptar($file['user'], 2));
+					$father      = base64_encode(Seguridad::encriptar($file['father'], 2));
+					$isShared    = ($this->_fs->isShared($file['data']['path']))? '<i class="fa fa-share-alt" title="Compartido" style="margin-left: 10px"></i> <i>'. $nombreCompartidor['nombre'] .' '. $nombreCompartidor['apellidos'] .'</i>' : '';
+
+					if($file['data']['type'] == 'dir'){
+
+						$files[] = [
+							'name'     => $file['data']['name'] . $isShared,
+							'icon'     => '<i class="fa fa-folder"></i>',
+							'size'     => '',
+							'type'     => $file['data']['type'],
+							'next'     => $next .'/'. $compartidor .'/'. $father,
+							'previous' => $previous];
+
+					} else {
+
+						$files[] = [
+							'name' => $file['data']['name'] . $isShared,
+							'icon' => '<div class="file-icon" data-type="'. $extension .'"></div>',
+							'size' => $size,
+							'type' => $file['data']['type'],
+							'next' => $next .'/'. $compartidor .'/'. $father];
+
+					}
+
+				}
+
+				$nombreCarpetaActual = str_replace('_', ' ', FS::getFolderName($folder));
+
+			// FIN DEL SISTEMA DE ARCHIVOS
+
+			$data = ['title' => 'Carpetas Compartidas'];
+
+			$section = [
+				'files'        => $files,
+				'previous'     => $previous,
+				'actual'       => $actual,
+				'titleSection' => (empty($nombreCarpetaActual))? 'Carpetas Compartidas Conmigo' : $nombreCarpetaActual];
+			
+			Session::set('template', 'user');
+
+			View::rendertemplate('header', $data);
+			View::rendertemplate('topHeader', $this->templateData);
+			View::rendertemplate('aside', $this->templateData);
+			View::render('user/folders/shared', $section);
+			View::rendertemplate('footer');
+
+		}
+
+	}
+
+/*
+|-----------------------------------------------
 | Formulario para Nueva Carpeta.
 |-----------------------------------------------
 */
@@ -409,7 +547,7 @@ class Folders extends \core\controller{
 |-----------------------------------------------
 */
 
-	public function download($file = '')
+	public function download($file = '', $userCompartidor = '', $father = '')
 	{
 
 		if(!$this->_user->isLogged()){
@@ -420,39 +558,42 @@ class Folders extends \core\controller{
 
 			$fileDecrypted = Seguridad::desencriptar(base64_decode($file), 2);
 
-			if(empty($fileDecrypted)){
-
+			if(empty($fileDecrypted))
 				Url::redirect('folders/');
 
-			} else {
+			if(!empty($userCompartidor) && !empty($father)){
 
-				if(FS::comprobeFolder($fileDecrypted)){
+				$userCompartidor = Seguridad::desencriptar(base64_decode($userCompartidor), 2);
 
-					$name = date('d-m-Y-h-i-s');
+				FS::personalFS($userCompartidor);
 
-					$carpetaAnterior = FS::getAnteriorPath($fileDecrypted);
+			}
 
-					if(FS::comprimeFolder($fileDecrypted, $name) === true){
+			if(FS::comprobeFolder($fileDecrypted)){
 
-						if(FS::download($name . '.zip')){
+				$name = date('d-m-Y-h-i-s');
 
-							$this->_log->add('Ha descargado una Carpeta Personal.');
+				$carpetaAnterior = FS::getAnteriorPath($fileDecrypted);
 
-						} else {
+				if(FS::comprimeFolder($fileDecrypted, $name) === true){
 
-							Url::redirect('folders/'. $carpetaAnterior);
+					if(FS::download($name . '.zip')){
 
-						}
+						$this->_log->add('Ha descargado una Carpeta.');
+
+					} else {
+
+						Url::redirect('folders/');
 
 					}
 
-				} else {
-
-					FS::download($fileDecrypted, false, false);
-
-					$this->_log->add('Ha descargado un Archivo Personal.');
-
 				}
+
+			} else {
+
+				FS::download($fileDecrypted, false, false);
+
+				$this->_log->add('Ha descargado un Archivo.');
 
 			}
 
