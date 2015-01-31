df6dbcd7ac19e0c4b7799b155ffd422fefd19610
angelblxt <angelquerolgarcia@gmail.com>
2015-01-31 23:17:06 +0100

    Optimizaciones en el Código.

--- index.php
+++ index.php
@@ -107,8 +107,8 @@ use \core\router,
 	Router::any('folders/(:any)/share', '\controllers\folders@share');
 	Router::any('folders/(:any)/unshare/(:any)', '\controllers\folders@unshare');
 	Router::any('cloud', '\controllers\folders@index');
-	Router::any('shared', '\controllers\folders@shared');
-	Router::any('shared/(:any)/(:any)/(:any)', '\controllers\folders@shared');
+	Router::any('shared', '\controllers\folders@folders');
+	Router::any('shared/(:any)/(:any)/(:any)', '\controllers\folders@folders');
 	Router::any('shared/(:any)/(:any)/(:any)/download', '\controllers\folders@download');
 
 	// Router::any('register', '\controllers\user@register');
