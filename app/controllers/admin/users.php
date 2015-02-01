ab344d7e51cd25ccea972c81f261d35d7792e46a
angelblxt <angelquerolgarcia@gmail.com>
2015-02-01 22:27:45 +0100

    Nuevos LOGS.

--- app/controllers/admin/users.php
+++ app/controllers/admin/users.php
@@ -66,6 +66,8 @@ class Users extends \core\controller{
 
 		} else {
 
+			$this->_log->add('Ha entrado en la sección de "Listado de Usuarios".');
+
 			// PAGINADOR DE USUARIOS //
 
 				$pages = new \helpers\paginator('20', 'p');
@@ -145,6 +147,151 @@ class Users extends \core\controller{
 			}
 
 			CSV::exportar('users-'. date('dmYHis'));
+
+			$this->_log->add('Ha descargado un archivo CSV de Usuarios.');
+
+		}
+
+	}
+
+	public function delete($hash, $confirm = 0)
+	{
+
+		if(!$this->_user->isLogged() || $this->templateData['isAdmin'] === false){
+
+			Url::redirect('');
+
+		} else {
+
+			$name = $this->_user->getNameSurname($this->_user->getUser($hash));
+
+			if($confirm == 0){
+
+				$data = ['title' => 'Eliminar Usuario'];
+
+				$section = [
+					'hash' => $hash,
+					'name' => $name];
+
+				Session::set('template', 'user');
+
+				View::rendertemplate('header', $data);
+				View::rendertemplate('topHeader', $this->templateData);
+				View::rendertemplate('aside', $this->templateData);
+				View::render('admin/users/delete', $section);
+				View::rendertemplate('footer');
+
+			} else {
+
+				if($this->_user->delete($hash)){
+
+					$this->_log->add('Ha eliminado del Sistema a '. $name .'.');
+
+					$_SESSION['error'] = ['El Usuario ha sido eliminado con éxito.', 'bien'];
+
+				} else {
+
+					$_SESSION['error'] = ['¡Oops! Hubo un error al intentar hacer eso.', 'mal'];
+
+				}
+
+				Url::redirect('admin/users');
+
+			}
+
+		}
+
+	}
+
+	public function edit($hash)
+	{
+
+		if(!$this->_user->isLogged() || $this->templateData['isAdmin'] === false){
+
+			Url::redirect('');
+
+		} else {
+
+			$user = $this->_user->getUser($hash);
+
+			$data = ['title' => 'Editar Usuario'];
+
+			$section = [
+				'hash'   => $hash,
+				'name'   => $this->_user->getNameSurname($user),
+				'user'   => $user,
+				'cursos' => System::cursos(),
+				'curso'  => $this->_user->getCurso($user),
+				'rangos' => ['Alumno', 'Profesor', 'Administrador'],
+				'rango'  => $this->_user->getRank($user),
+				'token'  => NoCSRF::generate('token')];
+
+			Session::set('template', 'user');
+
+			View::rendertemplate('header', $data);
+			View::rendertemplate('topHeader', $this->templateData);
+			View::rendertemplate('aside', $this->templateData);
+			View::render('admin/users/edit', $section);
+			View::rendertemplate('footer');
+
+		}
+
+	}
+
+	public function postEditUser()
+	{
+
+		if(!$this->_user->isLogged()){
+
+			Url::redirect('');
+
+		} else {
+
+			if(isset($_POST['edit']) && NoCSRF::check( 'token', $_POST, false, 60*10, false ) === true){
+
+				$hash      = $_POST['hash'];
+				$nombre    = $_POST['nombre'];
+				$apellidos = $_POST['apellidos'];
+				$nuevoUser = $_POST['user'];
+				$password  = $_POST['password'];
+				$curso     = $_POST['curso'];
+				$rango     = $_POST['rango'];
+
+				if(empty($nombre) || empty($apellidos) || empty($nuevoUser) || empty($curso) || empty($rango)){
+
+					$_SESSION['error'] = ['Hay campos que no puedes dejar vacíos.', 'precaucion'];
+
+				} elseif(!$this->_user->exists($hash)) {
+
+					$_SESSION['error'] = ['¡Oops! Hubo un error al intentar hacer eso.', 'mal'];
+
+				} else {
+
+					$user = $this->_user->getUser($hash);
+
+					$password = (empty($password))? $this->_user->getPassword($user) : hash('sha512', $password);
+
+					if($this->_user->edit($hash, $nuevoUser, $nombre, $apellidos, $password, $curso, $rango)){
+
+						$this->_log->add('Ha editado Información de '. $nombre .' '. $apellidos .'.');
+
+						$_SESSION['error'] = ['El Usuario ha sido editado con éxito.', 'bien'];
+
+					} else {
+
+						$_SESSION['error'] = ['¡Oops! Hubo un error al intentar hacer eso.', 'mal'];
+
+					}
+
+				}
+
+				Url::redirect('admin/users/'. $hash .'/edit');
+
+			} else {
+
+				Url::redirect('');
+
+			}
 
 		}
 
