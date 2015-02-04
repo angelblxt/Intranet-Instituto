18517191dd2db57d2d6214af36f9683b161ba214
angelblxt <angelquerolgarcia@gmail.com>
2015-02-04 22:14:47 +0100

    Cambio de la fecha del CopyRight. "2014" > "2015".

--- app/templates/user/footer.php
+++ app/templates/user/footer.php
@@ -2,7 +2,7 @@
 
 					</div>
 
-					<div class="copyright"> 2014 - Desarrollado por Ángel Querol García. </div>
+					<div class="copyright"> 2015 - Desarrollado por Ángel Querol García. </div>
 
 				</div>
 
